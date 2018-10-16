7d94a2216461fbb40dcc920b7db96374ca3c7fbb
Ankrad <25014241+Sank6@users.noreply.github.com>
2018-10-16 18:21:10 +0100

    Create botinfo.js

---
+++ commands/Bots/botinfo.js
@@ -0,0 +1,37 @@
+const { Command } = require('klasa');
+const { MessageEmbed } = require('discord.js');
+var Manager = require('../../manage.js');
+
+module.exports = class extends Command {
+    constructor(...args) {
+        super(...args, {
+            name: 'botinfo',
+            runIn: ['text'],
+            aliases: ["bot-info", "info"],
+            permLevel: 0,
+            botPerms: ["SEND_MESSAGES"],
+            requiredSettings: [],
+            description: "Get information for a bot",
+            usage: '(User:user)',
+            usageDelim: undefined
+        });
+    }
+
+    async run(message, [user]) {
+      if (!user.bot) return message.channel.send(`Ping a **bot** to get info about.`);
+      if (message.client.isMentioned) return message.channel.send(`-_- No`);
+      
+      Manager.fetch(user.id).then(ans => {
+        if (ans === false) return message.channel.send(`Bot not found.`)
+        ans = ans.bot;
+        let e = new MessageEmbed()
+        .setColor(0x6b83aa)
+        .setAuthor(ans.name, ans.logo, ans.invite)
+        .setDescription(ans.description)
+        .addField(`Prefix`, ans.prefix, true)
+        .addField(`Owner`, `<@${ans.owner}>`, true)
+        .addField(`State`, ans.state.capitalize(), true)
+        message.channel.send(e);
+      }).catch(er => {console.error(er)})
+    }
+};
