42e9cea6edfd01eff3c3ddea5adb8e4b3ee88672
Ankrad <25014241+Ankrad@users.noreply.github.com>
2019-07-30 13:39:37 +0100

    Add mongo db

--- providers/mongodb.js
+++ providers/mongodb.js
@@ -4,86 +4,87 @@ const { MongoClient: Mongo } = require('mongodb');
 
 module.exports = class extends Provider {
 
-	constructor(...args) {
-		super(...args, { description: 'Allows use of MongoDB functionality throughout Klasa' });
-		this.db = null;
-	}
+    constructor(...args) {
+        super(...args, { description: 'Allows use of MongoDB functionality throughout Klasa' });
+        this.db = null;
+    }
 
-	async init() {
-    const mongoClient = await Mongo.connect(process.env.MongoDB, { useNewUrlParser: true });
-		this.db = mongoClient.db(this.client.options.providers.db);
-	}
+    async init() {
+        const mongoClient = await Mongo.connect(process.env.MONGO_DB_URL, { useNewUrlParser: true });
+        this.db = mongoClient.db(this.client.options.providers.db);
+    }
 
-	/* Table methods */
+    /* Table methods */
 
-	get exec() {
-		return this.db;
-	}
+    get exec() {
+        return this.db;
+    }
 
-	hasTable(table) {
-		return this.db.listCollections().toArray().then(collections => collections.some(col => col.name === table));
-	}
+    hasTable(table) {
+        return this.db.listCollections().toArray().then(collections => collections.some(col => col.name === table));
+    }
 
-	createTable(table) {
-		return this.db.createCollection(table);
-	}
+    createTable(table) {
+        return this.db.createCollection(table);
+    }
 
-	deleteTable(table) {
-		return this.db.dropCollection(table);
-	}
+    deleteTable(table) {
+        return this.db.dropCollection(table);
+    }
 
-	/* Document methods */
+    /* Document methods */
 
-	getAll(table, filter = []) {
-		if (filter.length) return this.db.collection(table).find({ id: { $in: filter } }, { _id: 0 }).toArray();
-		return this.db.collection(table).find({}, { _id: 0 }).toArray();
-	}
+    getAll(table, filter = []) {
+        if (filter.length) return this.db.collection(table).find({ id: { $in: filter } }, { _id: 0 }).toArray();
+        return this.db.collection(table).find({}, { _id: 0 }).toArray();
+    }
 
-	getKeys(table) {
-		return this.db.collection(table).find({}, { id: 1, _id: 0 }).toArray();
-	}
+    getKeys(table) {
+        return this.db.collection(table).find({}, { id: 1, _id: 0 }).toArray();
+    }
 
-	get(table, id) {
-		return this.db.collection(table).findOne(resolveQuery(id));
-	}
+    get(table, id) {
+        return this.db.collection(table).findOne(resolveQuery(id));
+    }
 
-	has(table, id) {
-		return this.get(table, id).then(Boolean);
-	}
+    has(table, id) {
+        return this.get(table, id).then(Boolean);
+    }
 
-	getRandom(table) {
-		return this.db.collection(table).aggregate({ $sample: { size: 1 } });
-	}
+    getRandom(table) {
+        return this.db.collection(table).aggregate({ $sample: { size: 1 } });
+    }
 
-	create(table, id, doc = {}) {
-		return this.db.collection(table).insertOne(mergeObjects(this.parseUpdateInput(doc), resolveQuery(id)));
-	}
+    create(table, id, doc = {}) {
+        return this.db.collection(table).insertOne(mergeObjects(this.parseUpdateInput(doc), resolveQuery(id)));
+    }
 
-	delete(table, id) {
-		return this.db.collection(table).deleteOne(resolveQuery(id));
-	}
+    delete(table, id) {
+        return this.db.collection(table).deleteOne(resolveQuery(id));
+    }
 
-	update(table, id, doc) {
-		return this.db.collection(table).updateOne(resolveQuery(id), { $set: isObject(doc) ? flatten(doc) : parseEngineInput(doc) });
-	}
+    update(table, id, doc) {
+        return this.db.collection(table).updateOne(resolveQuery(id), { $set: isObject(doc) ? flatten(doc) : parseEngineInput(doc) });
+    }
 
-	replace(table, id, doc) {
-		return this.db.collection(table).replaceOne(resolveQuery(id), this.parseUpdateInput(doc));
-	}
+    replace(table, id, doc) {
+        return this.db.collection(table).replaceOne(resolveQuery(id), this.parseUpdateInput(doc));
+    }
 
 };
 
 const resolveQuery = query => isObject(query) ? query : { id: query };
 
 function flatten(obj, path = '') {
-	let output = {};
-	for (const [key, value] of Object.entries(obj)) {
-		if (isObject(value)) output = Object.assign(output, flatten(value, path ? `${path}.${key}` : key));
-		else output[path ? `${path}.${key}` : key] = value;
-	}
-	return output;
+    let output = {};
+    for (const [key, value] of Object.entries(obj)) {
+        if (isObject(value)) output = Object.assign(output, flatten(value, path ? `${path}.${key}` : key));
+        else output[path ? `${path}.${key}` : key] = value;
+    }
+    return output;
 }
 
 function parseEngineInput(updated) {
-	return Object.assign({}, ...updated.map(entry => ({ [entry.data[0]]: entry.data[1] })));
+    return Object.assign({}, ...updated.map(entry => ({
+        [entry.data[0]]: entry.data[1] })));
 }
