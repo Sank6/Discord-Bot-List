4e8f1c402726df3eb97b75d6b2580fbc7ba537fa
Ankrad <25014241+Ankrad@users.noreply.github.com>
2020-01-24 19:50:32 +0000

    Fix bug where everyone can edit bots

--- src/Routes/bots/edit.js
+++ src/Routes/bots/edit.js
@@ -6,6 +6,7 @@ route.get("/:id", async (req, res, next) => {
     let bot = JSON.parse(req.app.get('client').settings.get('bots')).find(u => u.id === req.params.id);
 
     if (!bot) return res.sendStatus(404);
+    if (!bot.owners.includes(user.id)) return res.redirect(`/error?e=owner`);
     res.render("edit/index", { bot: bot });
 });
 
