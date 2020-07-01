370007edb2c9b24216dd00c945dd40d7d8bec1c0
Misly <60405462+Misly16@users.noreply.github.com>
2020-07-01 21:51:17 +0100

    Update login.js

--- src/routes/login.js
+++ src/routes/login.js
@@ -4,7 +4,7 @@ const { CLIENT_ID, DOMAIN } = process.env;
 const route = Router();
 
 route.get("/", async (req, res, next) => {
-    res.redirect(`https://discordapp.com/api/oauth2/authorize?client_id=${CLIENT_ID}&response_type=code&scope=identify&redirect_uri=${encodeURIComponent(DOMAIN)}/api/callback`);
+    res.redirect(`https://discord.com/api/oauth2/authorize?client_id=${CLIENT_ID}&response_type=code&scope=identify&redirect_uri=${encodeURIComponent(DOMAIN)}/api/callback`);
 });
 
 module.exports = route;
