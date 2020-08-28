36b4041d5d0357d99e54e0db7d1d468d1087bd19
idkwhattochoose#6147(discord) <63606830+idk-pixel@users.noreply.github.com>
2020-08-28 11:05:05 -0500

    Fix widget
    i think this should fix widget command

--- src/bot/commands/Bots/widget.js
+++ src/bot/commands/Bots/widget.js
@@ -11,11 +11,14 @@ module.exports = class extends Command {
         });
     }
 
-    async run(message, [user]) {
-        if (!user || !user.bot) return message.channel.send(`You didn't ping a bot to get a widget of.`);
+    async run(message) {
+    let user = message.mentions.members.first() || message.guild.members.cache.get(args[0])
+        if (!user.bot) return message.channel.send('Ping a **bot**')
+        if(user.bot){
         let url = `${domain_with_protocol}/embed/${user.id}`
         let img = await fetch(url).then(res => res.buffer());
         message.channel.send({ files: [img] });
+        }
     }
 
 };
