d608428a470226976804da47cd5740a6b17751f8
Sank6 <25014241+Sank6@users.noreply.github.com>
2020-08-28 11:58:11 +0100

    Fix bug where stat posting didn't work properly.

--- src/routes/api/auth/stats.js
+++ src/routes/api/auth/stats.js
@@ -20,7 +20,7 @@ route.post('/:id', async (req, res) => {
     if (!bot) return res.json({ success: "false", error: "Bot not found." });
     if (!bot.auth) return res.json({ success: "false", error: "Create a bot authorization token." });
     if (bot.auth !== auth) return res.json({ success: "false", error: "Incorrect authorization token." });
-    if (bot.servers.length > 0 && Date.now() - bot.servers[bot.servers.length-1].time < ratelimit * 1000) return res.json({ success: "false", error: "You are being ratelimited." });
+    if (bot.servers.length > 0 && bot.servers[bot.servers.length-1] &&  Date.now() - bot.servers[bot.servers.length-1].time < ratelimit * 1000) return res.json({ success: "false", error: "You are being ratelimited." });
     
     bot = await Bots.findOneAndUpdate({ botid }, {"$push":{"servers":{"$each": [{count}]}}}, { runValidators: true })
     res.json({ success: true, bot });
