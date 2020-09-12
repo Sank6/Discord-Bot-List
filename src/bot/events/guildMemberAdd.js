42a4d97307c7b48a076ca8a9bc223165e9046de0
idk-pixel <63606830+idk-pixel@users.noreply.github.com>
2020-09-12 10:10:49 -0500

    Update guildMemberAdd.js

--- src/bot/events/guildMemberAdd.js
+++ src/bot/events/guildMemberAdd.js
@@ -3,7 +3,12 @@ const { Event } = require('klasa');
 const { server: {role_ids} } = require("@root/config.json");
 
 module.exports = class extends Event {
-    run(member) {
+    run(message, member) b
+        const role = message.guild.roles.cache.find(role => role.name === 'Server Member')
+        if(!member.user.bot) {
+            if(!role) return;
+            message.member.roles.add(role)
+        }
         if (member.user.bot) {
             member.roles.add(member.guild.roles.cache.get(role_ids.bot));
             member.roles.add(member.guild.roles.cache.get(role_ids.unverified));
