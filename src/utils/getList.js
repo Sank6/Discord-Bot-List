cdd04df0483a6888b966aeea8b3763a7199164a4
Sank6 <25014241+Sank6@users.noreply.github.com>
2020-09-14 22:38:55 +0100

    Filter publicly accessible data

--- src/utils/getList.js
+++ src/utils/getList.js
@@ -1,6 +1,11 @@
 const Bots = require("@models/bots");
 
 module.exports = async () => {
-    const bots = await Bots.find({}, { _id: false, auth: false })
-    return bots.filter(bot => bot.state != "deleted")
+    const bots = await Bots.find({"state": {$ne: "deleted"}}, { _id: false, auth: false, __v: false, addedAt: false })
+    return bots.map(x => {
+        if (x.servers && x.servers[x.servers.length - 1])
+            x.servers = x.servers[x.servers.length - 1].count;
+        else x.servers = null
+        return x
+    })
 };
