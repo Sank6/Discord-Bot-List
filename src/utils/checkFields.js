02f0c94e753b9ff106b30e9204009149362fb2a6
Sank6 <25014241+Sank6@users.noreply.github.com>
2020-10-28 16:42:35 +0000

    Additional owners can edit bots

--- src/utils/checkFields.js
+++ src/utils/checkFields.js
@@ -7,11 +7,12 @@ module.exports = async (req, b=null) => {
     let data = req.body;
     if (data.description.length > 120) return {success: false, message: "Your summary is too long."};
     
-    let memberCheck = req.app.get('client').guilds.cache.get(id).member(req.user.id);
+    let memberCheck = await req.app.get('client').guilds.cache.get(id).members.fetch(req.user.id);
 
     let [bot] = await getBot(req.params.id);
-    if (memberCheck == null)
+    if (memberCheck == null) {
         return {success: false, message: "You aren't in the server", button: {text: "Join", url: "/join"}}
+    }
     if (bot.user_id && bot.user_id[0].endsWith("is not snowflake."))
         return {success: false, message: "Invalid bot id"}
     if (bot.message == "Unknown User" || bot.bot !== true)
@@ -24,7 +25,7 @@ module.exports = async (req, b=null) => {
     if (b && !b.owners.includes(req.user.id) && !server.admin_user_ids.includes(req.user.id))
         return {success: false, message: "Invalid request. Please sign in again.", button: {text: "Logout", url: "/logout"}}
 
-    if (b && data.owners.replace(',', '').split(' ').remove('').join() !== b.owners.join() && b.owners[0] !== req.user.id)
+    if (b && data.owners.replace(',', '').split(' ').remove('').join() !== b.owners.slice(1).join() && b.owners[0] !== req.user.id)
         return {success: false, message: "Only the primary owner can edit additional owners"};
 
     let users = [req.user.id];
