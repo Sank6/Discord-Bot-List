a4fa585a819c836d8c66fe7818adcd53fb53c2df
Sank6 <25014241+Sank6@users.noreply.github.com>
2020-10-31 02:45:42 +0000

    Fix recaptcha settiings

--- src/routes/add.js
+++ src/routes/add.js
@@ -1,9 +1,13 @@
 const { Router } = require("express");
 
+const { web: {recaptcha_v2: {site_key}} } = require("@root/config.json");
+
 const route = Router();
 
 route.get("/", async (req, res, next) => {
-    res.render("add", {user: req.user})
+    let theme = "light";
+    if (req.cookies["theme"] === "dark") theme = "dark"
+    res.render("add", {user: req.user, theme, site_key})
 });
 
 module.exports = route;
