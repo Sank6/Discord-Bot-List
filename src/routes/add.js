1896cedf7d17561acf923ce2c0ec8a2eebceba97
Sank6 <25014241+Sank6@users.noreply.github.com>
2020-11-14 21:18:12 +0000

    require auth for /add

--- src/routes/add.js
+++ src/routes/add.js
@@ -1,10 +1,11 @@
 const { Router } = require("express");
+const { auth } = require('@utils/discordApi');
 
 const { web: {recaptcha_v2: {site_key}} } = require("@root/config.json");
 
 const route = Router();
 
-route.get("/", async (req, res, next) => {
+route.get("/", auth, async (req, res) => {
     let theme = "light";
     if (req.cookies["theme"] === "dark") theme = "dark"
     res.render("add", {user: req.user, theme, site_key})
