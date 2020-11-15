3a90b0fb45dcd1a173f53a973a8cab90f14c4b6b
Sank6 <25014241+Sank6@users.noreply.github.com>
2020-11-15 16:03:43 +0000

    Deal with dates in the backend

--- src/public/assets/js/vote.js
+++ src/public/assets/js/vote.js
@@ -1,5 +1,5 @@
 $(document).ready(async function () {
-  $('#vote').click(() => {
+  $('#vote').click(async () => {
     const swalWithBootstrapButtons = Swal.mixin({
       customClass: {
         confirmButton: 'btn btn-success',
@@ -7,86 +7,36 @@ $(document).ready(async function () {
       },
       buttonsStyling: true
     })
-    swalWithBootstrapButtons.fire({
+    let { isConfirmed } = await swalWithBootstrapButtons.fire({
       title: 'Are you sure you want to vote this bot ?',
-      text: "You won't be able to vote for the next 12 hours!",
+      text: "You won't be able to vote for the next 12 hours.",
       icon: 'info',
       showCancelButton: true,
-      confirmButtonColor: '#28a745',
-      cancelButtonColor: '#d33',
-      confirmButtonText: 'Yes, Vote!',
-      cancelButtonText: 'No, Cancel!',
-      reverseButtons: false
-    }).then((result) => {
-      if (result.isConfirmed) {
-        if (document.getElementById('date').innerText == 'Undefined') {
-          const text = document.getElementById('votes').innerHTML.replace(' Votes', '')
-          const votes = Number(text) + 1
-          document.getElementById('votes').innerHTML = `${votes} Votes`;
-          document.getElementById('date').innerText = new Date();
-          let method = "PATCH";
-          let botId = location.href.split(location.host)[1].replace('/bots/vote/', '').replace('/', '');
-          fetch(`/api/vote/${botId}`, {
-            method,
-            headers: {
-              'Content-Type': 'application/json'
-            }
-          })
-          swalWithBootstrapButtons.fire({
-            title: 'Success',
-            text: 'You have successfully voted !',
-            icon: 'success',
-            confirmButtonColor: '#28a745'
-          })
-
-        } else {
-          const date = document.getElementById('date').innerText;
-          var dt = new Date(date)
-          dt.setHours(dt.getHours() + 12);
-          var countDownDate = dt.getTime();
-          var now = new Date().getTime();
-          var distance = countDownDate - now;
-          var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-          var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-          var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-          if (distance < 0) {
-            const text = document.getElementById('votes').innerHTML.replace(' Votes', '')
-            const votes = Number(text) + 1
-            document.getElementById('votes').innerHTML = `${votes} Votes`
-            document.getElementById('date').innerHTML = new Date();
-            let method = "PATCH";
-            let botId = location.href.split(location.host)[1].replace('/bots/vote/', '').replace('/', '');
-            fetch(`/api/vote/${botId}`, {
-              method,
-              headers: {
-                'Content-Type': 'application/json'
-              }
-            })
-            swalWithBootstrapButtons.fire({
-              title: 'Success',
-              text: 'You have successfullf voted !',
-              icon: 'success',
-              confirmButtonColor: '#28a745'
-            })
-          } else {
-            swalWithBootstrapButtons.fire({
-              title: 'Error',
-              text: `You can only vote again after ${hours} Hours ${minutes} Minutes ${seconds} Seconds !`,
-              icon: 'error',
-              confirmButtonColor: '#d33'
-            })
-          }
-        }
-      } else if (
-        result.dismiss === Swal.DismissReason.cancel
-      ) {
-        swalWithBootstrapButtons.fire({
-          title: 'Cancelled',
-          text: 'Voting Cancelled',
-          icon: 'error',
-          confirmButtonColor: '#d33'
-        })
-      }
+      confirmButtonText: 'Yes, Vote'
     })
+    if (!isConfirmed) return;
+    let botid = location.href.split(location.host)[1].replace('/bots/vote/', '').replace('/', '');
+    let req = await fetch(`/api/vote/${botid}`, {
+      method: "PATCH",
+      headers: { 'Content-Type': 'application/json' }
+    })
+    req = await req.json()
+    if (req.success) {
+      await swalWithBootstrapButtons.fire({
+        title: 'Success',
+        text: 'You have successfully voted !',
+        icon: 'success'
+      })
+      location.href = `/bots/${botid}`
+    } else {
+      let hours = 11 - Math.floor(req.time / 3600000);
+      let minutes = 60 - Math.ceil(req.time  / 60000);
+      await swalWithBootstrapButtons.fire({
+        title: 'Error',
+        text: `You can vote again after ${hours} hours and ${minutes} minutes`,
+        icon: 'error'
+      })
+      location.href = `/bots/${botid}`
+    }
   })
 })
--- src/routes/api/vote/index.js
+++ src/routes/api/vote/index.js
@@ -2,40 +2,25 @@ const { Router } = require("express");
 const { auth } = require("@utils/discordApi");
 const Bots = require("@models/bots");
 const Users = require("@models/users");
-const { server, web } = require("@root/config.json")
-const Discord = require("discord.js");
+const { server } = require("@root/config.json")
 
 const route = Router();
 
 route.patch("/:id", auth, async (req, res) => {
-  let vote = 1
-  let date = new Date();
-  await Bots.updateOne({ botid: req.params.id }, { $inc: { vote } })
-  const users = await Users.findOne({ userid: req.user.id }, { _id: false })
-  if (!users) {
-    new Users({
-      userid: req.user.id,
-      date: new Date()
-    }).save()
-  } else {
-    await Users.updateOne({ userid: req.user.id }, { $set: { date } });
-  }
+  let user = await Users.findOne({ userid: req.user.id })
+  if (user && (Date.now() - user.date.getTime()) < 43200000) 
+    return res.json({success: false, time: Date.now() - user.date.getTime()})
+
+  await Bots.updateOne({ botid: req.params.id }, { $inc: { vote: 1 } })
+  await Users.updateOne({ userid: req.user.id }, { $set: { date: new Date() } }, { upsert: true })
+
   let userProfile = await req.app.get('client').users.fetch(req.user.id);
-  let bot = await Bots.findOne({botid: req.params.id}, { _id: false, auth: false });
-  const embed = new Discord.MessageEmbed()
-    .setTitle('Vote Count Updated! 🎉')
-    .setColor('BLUE')
-    .setDescription(`The vote count for <@${req.params.id}> has been updated.`)
-    .addField(`Voter`, `<@${req.user.id}> (${userProfile.tag})`, false)
-    .addField(`Vote Count`, `${bot.vote} Votes`, false)
-    .addField(`Bot Page`, `[Here](${web.domain_with_protocol}/bots/${req.params.id})`)
-    .setTimestamp();
-  const channel = await req.app.get('client').channels.cache.get(server.vote_log);
-  const webhooks = await channel.fetchWebhooks();
-  const webhook = webhooks.first();
-  await webhook.send({
-    embeds: [embed],
-  });
+  
+  let channel = await req.app.get('client').channels.cache.get(server.vote_log);
+  let webhook = (await channel.fetchWebhooks()).first();
+  if (!webhook) 
+    webhook = await channel.createWebhook('Discord Bot List')
+  await webhook.send(`<@${req.user.id}> (${userProfile.tag}) has voted for <@${req.params.id}>`);
   return res.json({ success: true })
 });
 
