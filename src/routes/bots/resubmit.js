b80ae4560101dc63a2cf4aec14bcbf2e1e3de908
Sank6 <25014241+Sank6@users.noreply.github.com>
2020-11-16 18:17:28 +0000

    Fix error page renders

--- src/routes/bots/resubmit.js
+++ src/routes/bots/resubmit.js
@@ -9,8 +9,8 @@ const route = Router();
 route.get("/:id", auth, async (req, res) => {
     let bot = await Bots.findOne({botid: req.params.id}, { _id: false })
 
-    if (!bot) return res.render(404);
-    if (bot.state !== "deleted") return res.render(404);
+    if (!bot) return res.render("404");
+    if (bot.state !== "deleted") return res.render("404");
     let theme = "light";
     if (req.cookies["theme"] === "dark") theme = "dark"
     res.render("resubmit", { bot: bot, user: req.user, theme, site_key });
