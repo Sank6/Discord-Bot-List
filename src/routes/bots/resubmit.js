b9562f9200e1be589e6957b8cf43eeb5a0401b7c
Sank6 <25014241+Sank6@users.noreply.github.com>
2020-12-22 23:00:01 +0000

    Add min/max lengths for description + customisable

--- src/routes/bots/resubmit.js
+++ src/routes/bots/resubmit.js
@@ -2,7 +2,7 @@ const { Router } = require("express");
 const { auth } = require('@utils/discordApi')
 const Bots = require("@models/bots");
 
-const { web: {recaptcha_v2: {site_key}}, bot_options: {bot_tags}} = require("@root/config.json");
+const { web: {recaptcha_v2: {site_key}}, bot_options: {bot_tags, max_summary_length}} = require("@root/config.json");
 
 const route = Router();
 
@@ -16,6 +16,7 @@ route.get("/:id", auth, async (req, res) => {
         bot,
         user: req.user,
         bot_tags,
+        max_summary_length,
         site_key,
         req
     });
