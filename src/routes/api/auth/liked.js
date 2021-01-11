61ad38f2e1cd000414210745e97720c1cc7456f3
Akurejs <41219922+Akurejs@users.noreply.github.com>
2021-01-11 18:39:26 -0300

    fixes duplicate key error collection #458
    fixes the mongodb duplicate key error collection problem

--- src/routes/api/auth/liked.js
+++ src/routes/api/auth/liked.js
@@ -8,7 +8,7 @@ const route = Router();
 route.use(bodyParser.json({ limit: '50mb' }));
 
 route.get('/:id', authApi, async (req, res) => {
-    let unfiltered = await Users.find({botliked: req.params.id}, { _id: false, __v: false, botliked: false });
+    let unfiltered = await Users.find({ userid: req.user.id });
     let users = []
     for (let user of unfiltered) {
         if (user && (Date.now() - user.time) < 43200000) 
