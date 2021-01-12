887de921b7abc0e8ed1d80461b6ded3c9da369be
Sank6 <25014241+Sank6@users.noreply.github.com>
2021-01-12 23:34:31 +0000

    Add verified badge to embed

--- src/routes/api/embed.js
+++ src/routes/api/embed.js
@@ -4,6 +4,8 @@ const Bots = require("@models/bots");
 
 const { web: {domain_with_protocol}, server: {id} } = require("@root/config.json");
 
+const path = require("path");
+
 const route = Router();
 
 route.get("/:id", async (req, res) => {
@@ -13,22 +15,27 @@ route.get("/:id", async (req, res) => {
     let owner = await req.app.get("client").guilds.cache.get(id).members.fetch(bot.owners.primary);
     let lg = decodeURIComponent(bot.logo.replace("/avatar/?avatar=", ""))
     let avatar = await resolveImage(lg);
+    let verified = await resolveImage(path.join(__dirname, "./verified_badge.png"));
+
+    let discord_verified = (await (await req.app.get('client').users.fetch(req.params.id)).fetchFlags()).has("VERIFIED_BOT");
 
     let img = new Canvas(500, 250)
       .setColor("#404E5C")
       .printRectangle(0, 0, 500, 250)
       .setColor("#DCE2F9")
-      .setTextAlign("center")
       .setTextFont('bold 35px sans')
-      .printText(bot.username, 250, 67)
-      .printRoundedImage(avatar, 20, 20, 70, 70, 20, true)
+      .printText(bot.username, 120, 75)
+      .printRoundedImage(avatar, 30, 30, 70, 70, 20)
       .setTextAlign("left")
       .setTextFont('bold 12px Verdana')
-    if (bot.servers[bot.servers.length-1]) img.printText(`${bot.servers[bot.servers.length-1].count} servers | ${bot.likes} ❤️`, 20, 125);
+    if (bot.servers[bot.servers.length-1])
+      img.printText(`${bot.servers[bot.servers.length-1].count} servers | ${bot.likes} ❤️`, 30, 125);
+    if (discord_verified)
+      img.printImage(verified, 420, 55)
     img
-      .printText(`Prefix: ${bot.prefix}`, 20, 145)
+      .printText(`Prefix: ${bot.prefix}`, 30, 145)
       .setTextFont('normal 15px Verdana')
-      .printWrappedText(bot.description, 20, 175, 400, 15)
+      .printWrappedText(bot.description, 30, 175, 440, 15)
       
       .setTextFont('bold 12px sans-serif')
       .printText(owner.user.tag, 10, 245)
