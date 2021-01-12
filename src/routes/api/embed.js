40e6c828949f3ab179e592fa269b5eba43208fee
Sank6 <25014241+Sank6@users.noreply.github.com>
2021-01-12 23:11:40 +0000

    Update embed

--- src/routes/api/embed.js
+++ src/routes/api/embed.js
@@ -14,31 +14,26 @@ route.get("/:id", async (req, res) => {
     let lg = decodeURIComponent(bot.logo.replace("/avatar/?avatar=", ""))
     let avatar = await resolveImage(lg);
 
-    let img = new Canvas(500, 200)
-      .setColor("#ffffff")
-      .printRectangle(0, 0, 500, 200)
-      .setColor("#888888")
-      .printRoundedRectangle(39, 29, 422, 52, 2)
-      .setColor("#ffffff")
-      .printRoundedRectangle(40, 30, 420, 50, 2)
-      .setColor("#888888")
+    let img = new Canvas(500, 250)
+      .setColor("#404E5C")
+      .printRectangle(0, 0, 500, 250)
+      .setColor("#DCE2F9")
       .setTextAlign("center")
-      .setTextSize(35)
-      .printText(bot.username, 250, 67)
-      .printCircularImage(avatar, 80, 135, 40, 40, 5, true)
+      .setTextFont('bold 35px sans')
+      .printText(`${bot.username}`, 250, 67)
+      .printRoundedImage(avatar, 20, 20, 70, 70, 20, true)
       .setTextAlign("left")
-      .setTextSize(12);
-    if (bot.servers[bot.servers.length-1]) img.printText(`${bot.servers[bot.servers.length-1].count} servers`, 140, 105);
+      .setTextFont('bold 12px Verdana')
+    if (bot.servers[bot.servers.length-1]) img.printText(`${bot.servers[bot.servers.length-1].count} servers | ${bot.likes} ❤️`, 20, 125);
     img
-      .printText(`Prefix: ${bot.prefix}`, 140, 125)
-      .setTextSize(11)
-      .printWrappedText(bot.description, 140, 145, 320, 15)
-
-      .setTextSize(10)
+      .printText(`Prefix: ${bot.prefix}`, 20, 145)
+      .setTextFont('normal 15px Verdana')
+      .printWrappedText(bot.description, 20, 175, 400, 15)
+      
+      .setTextFont('bold 12px sans-serif')
+      .printText(owner.user.tag, 10, 245)
       .setTextAlign("right")
-      .printText(domain_with_protocol, 490, 195)
-      .setTextAlign("left")
-      .printText(owner.user.tag, 10, 195);
+      .printText(domain_with_protocol, 490, 245);
 
     res.writeHead(200, {
       "Content-Type": "image/png"
