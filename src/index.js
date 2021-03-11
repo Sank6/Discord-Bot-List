103b17580f0991e79d00bb811941b71a308d1006
Hideaki <53636771+HideakiAtsuyo@users.noreply.github.com>
2021-03-11 18:11:12 +0100

    80 => 8080

--- src/index.js
+++ src/index.js
@@ -15,7 +15,6 @@ const { web: {port}, discord_client: {token}, mongo_url } = require("@root/confi
     });
     console.log(`Connected to the database on`,`\x1b[34m\x1b[4m${mongo_url}\x1b[0m`);
     let client = await bot.init(token);
-    console.log(`Logged in as ` + `\x1b[34m\x1b[4m${client.user.tag}\x1b[0m`);
     await new App(client).listen(port || 8080);
-    console.log(`Running on port ` + `\x1b[34m\x1b[4m${port || 80}\x1b[0m`);
+    console.log(`Running on port ` + `\x1b[34m\x1b[4m${port || 8080}\x1b[0m`);
 })()
