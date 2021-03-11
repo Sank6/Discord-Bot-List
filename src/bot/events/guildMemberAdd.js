e3cc39af70e41322ba82c008a229e6f57a8dc065
Hideaki <53636771+HideakiAtsuyo@users.noreply.github.com>
2021-03-11 18:44:16 +0100

    Need to test but pretty sure that it's working
    Sorry i was with v11 lol not sure for v12

--- src/bot/events/guildMemberAdd.js
+++ src/bot/events/guildMemberAdd.js
@@ -5,8 +5,10 @@ const { server: {role_ids} } = require("@root/config.json");
 module.exports = class extends Event {
     run(member) {
         if (member.user.bot) {
-            member.roles.add(member.guild.roles.cache.get(role_ids.bot));
-            member.roles.add(member.guild.roles.cache.get(role_ids.unverified));
+            member.rolescache.add([role_ids.bot, role_ids.unverified]);
+            /*member.roles.add(member.guild.roles.cache.get(role_ids.bot));
+            member.roles.add(member.guild.roles.cache.get(role_ids.unverified));*/
+ 
         }
     }
 };
