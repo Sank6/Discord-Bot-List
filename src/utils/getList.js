5e29a8fd763338fd288fcbffff6a2281a769be6d
Hideaki <53636771+HideakiAtsuyo@users.noreply.github.com>
2021-03-11 18:19:17 +0100

    Update getList.js

--- src/utils/getList.js
+++ src/utils/getList.js
@@ -4,8 +4,7 @@ module.exports = async () => {
     let bots = await Bots.find({"state": {$ne: "deleted"}}, { _id: false, auth: false, __v: false, addedAt: false })
     bots.sort((a, b) => b.likes - a.likes);
     return bots.map(x => {
-        if (x.servers && x.servers[x.servers.length - 1])
-            x.servers = x.servers[x.servers.length - 1].count;
+        if (x.servers && x.servers[x.servers.length - 1]) x.servers = x.servers[x.servers.length - 1].count;
         else x.servers = null
         return x
     })
